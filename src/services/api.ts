e3410a2763a2adb6777705207a34546ee38fcf2c
Amal Thomas <amalthomas333444@gmail.com>
2026-01-02 00:04:58 +0530

    trying to host in netlify4

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://18.60.239.13", // IMPORTANT
-  baseURL: "/api", // IMPORTANT
+  baseURL: "/", // IMPORTANT
   withCredentials: true,
   headers: { "Content-Type": "application/json" }
 });
